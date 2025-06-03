Fixed sampeling and added first vacf computation

poreana/sample.py
@@ -1073,8 +1073,6 @@ class Sample:
             Direction of descretization of the simulation box (**0** (x-axis);
             **1** (y-axis); **2** (z-axis))
         """
-        # TODO write documentation
-        # TODO overthink the input variables
         # TODO implement different bin options
         # TODO timereversal mapping
         # Initialize
@@ -1105,8 +1103,8 @@ class Sample:
         # Calculate bins
         bins = self._bin_mc(bin_num, direction)["bins"]
 
-        print("Corr_steps", corr_steps)
-        print("new_time_origin_steps", new_time_origin_steps)
+        print("Corr_steps", round(corr_steps))
+        print("new_time_origin_steps", round(new_time_origin_steps))
 
         # Create input dictionary
         self._diff_vacf_inp = {"output": link_out,"bins": bins, "len_correration": len_correration,  
@@ -1435,6 +1433,8 @@ class Sample:
         elif self._is_diffusion_vacf:
             output["diffusion_vacf"] = self._diffusion_vacf_data()
 
+        # velos = np.zeros((frame_list[-1]+1, len(self._res_list), 3), float)
+
         # Load trajectory
         traj = cf.Trajectory(self._traj)
         frame_form = "%"+str(len(str(self._num_frame)))+"i"
@@ -1471,17 +1471,22 @@ class Sample:
             # Run through residues
             for res_id in self._res_list:
                 # Get position and velocity vectors
-                pos = [[positions[self._res_list[res_id][atom_id]][i]/10+shift[i] for i in range(3)] for atom_id in range(len(self._atoms))]
+                pos = [[positions[self._res_list[res_id][atom_id]][i]/10+shift[i] 
+                        for i in range(3)] for atom_id in range(len(self._atoms))]
                 if self._is_diffusion_vacf:
-                    vel = [[velocities[self._res_list[res_id][atom_id]][i]/10 for i in range(3)] for atom_id in range(len(self._atoms))]
-                # TODO                                                    #### need /10?  
+                    vel = [[velocities[self._res_list[res_id][atom_id]][i]*100 
+                            for i in range(3)] for atom_id in range(len(self._atoms))]
+                # TODO WTF why A and not nm???                         A/ps -> m/s
 
                 # Calculate centre of mass
-                com_no_pbc = [sum([pos[atom_id][i]*self._masses[atom_id] for atom_id in range(len(self._atoms))])/self._sum_masses for i in range(3)]
+                com_no_pbc = [sum([pos[atom_id][i]*self._masses[atom_id] for atom_id 
+                                   in range(len(self._atoms))])/self._sum_masses for i in range(3)]
 
                 # Calculate velocity of centre of mass
                 if self._is_diffusion_vacf:
-                    vel_com = [sum([vel[atom_id][i]*self._masses[atom_id] for atom_id in range(len(self._atoms))])/self._sum_masses for i in range(3)]
+                    vel_com = [sum([vel[atom_id][i]*self._masses[atom_id] for atom_id 
+                                    in range(len(self._atoms))])/self._sum_masses for i in range(3)]
+                    # velos[frame_id][res_id] = vel_com
 
                 # Check if molecule is broken
                 if is_broken:
@@ -1553,5 +1558,7 @@ class Sample:
             if (frame_id+1)%10==0 or frame_id==0 or frame_id==self._num_frame-1:
                 sys.stdout.write("Finished frame "+frame_form%(frame_id+1)+"/"+frame_form%self._num_frame+"...\r")
                 sys.stdout.flush()
+
+        # np.save(self._traj.split(".")[0]+"_velos.npy", velos)
         print()
         return output

poreana/vacf.py
@@ -1,10 +1,23 @@
 import math
 import numpy as np
-from scipy.integrate import cumulative_trapezoid, cumulative_simpson
+from scipy.integrate import cumulative_simpson
 
 import poreana.utils as utils
 
 def vacf(link_data):
+    """
+    Calculate the velocity autocorrelation function (VACF) from the given data.
+    The VACF is calculated using the cumulative Simpson rule for integration.
+
+    Parameters
+    ----------
+    link_data : str
+        The path to the data file containing the VACF data.
+    Returns
+    -------
+    integrated : np.ndarray
+        The integrated VACF data, with shape (bin_num, num_res, corr_steps, 3).
+    """
     sample = utils.load(link_data)
 
     data = sample["data"]
@@ -23,29 +36,31 @@ def vacf(link_data):
     new_time_origin_steps = inp["new_time_origin_steps"]
     num_res = inp["num_res"]
 
-    box = sample["box"]["length"]
-
-    directions = [i for i in range(3) if i!= direction]
-    surface = np.array(box[directions[0]]*box[directions[1]])
-    distances = np.diff(bins, axis=0)
-    volumes = surface * distances
-
     num_new_time_origins = sum([data[bin]["density"] for bin in range(bin_num)]) / num_res
+    print("Averaged over ", num_new_time_origins, " new time origins.")
 
     avg_res_per_bin = [data[bin]["density"] / num_new_time_origins for bin in range(bin_num)]
-
-    # density = [avg_res_per_bin[bin] / volumes[bin] for bin in range(bin_num)]
+    print("Average number of residues per bin:", avg_res_per_bin)
+    avg_res_per_bin = np.where(avg_res_per_bin == 0, np.nan, avg_res_per_bin)
 
     vacf_data = np.zeros((bin_num, num_res, corr_steps, 3))
     for bin in range(bin_num):
+        # vacf_data[bin] = np.array([data[bin][res_id] for res_id in data[bin] if isinstance(res_id, int)])
+        # vacf_data[bin] *= num_res / data[bin]["density"]
         vacf_data[bin] = np.array([data[bin][res_id] for res_id in data[bin] if isinstance(res_id, int)]) / num_new_time_origins
         vacf_data[bin] *= num_res / avg_res_per_bin[bin]
-
-    integrated_1 = np.zeros(vacf_data.shape)
-    integrated_2 = np.zeros(vacf_data.shape)
+    
+    integrated = np.zeros(vacf_data.shape)
 
     for bin in range(bin_num):
         for res_id in range(num_res):
             for dim in range(3):
-                integrated_1[bin, res_id, :, dim] = cumulative_simpson(vacf_data[bin, res_id, :, dim], dx=len_frame, initial=0)
-                integrated_2[bin, res_id, :, dim] = cumulative_trapezoid(vacf_data[bin, res_id, :, dim], dx=len_frame, initial=0)
+                integrated[bin, res_id, :, dim] = cumulative_simpson(
+                    vacf_data[bin, res_id, :, dim], dx=len_frame * sample_step, initial=0)
+
+    return integrated
+"""
+        vacf * num_res                                          vacf * num_res
+erg = -------------------------------------------------- = ----------------------------------------------
+        num_new_time_origins * avg_res_per_bin                  res_per_bin
+"""